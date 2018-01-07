8e7d734792b4eda69ff2c6557f3d6411b02e855d
Anders Wiberg Olsen <anders@wiberg.tech>
2018-01-07 23:16:26 +0100

    Add rwords library and finish project

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const http = require('http')
+const rword = require('rword')
 const port = 3000
 
 const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello Node.js Server!')
+    let croppedUrl = request.url.substring(1)
+
+    if (request.url.startsWith("/json/")) {
+        croppedUrl = request.url.substring(6)
+    }
+
+    const count = parseInt(croppedUrl)
+
+    if (isNaN(count)) {
+        response.end("Not a number...")
+    } else {
+        let words = rword.generate(count)
+
+        let sentence = ""
+
+        for (let word of words) {
+            sentence += word + " "
+        }
+
+        if (request.url.startsWith("/json/")) {
+            response.end(JSON.stringify({ words }))
+        } else {
+            response.end(sentence)
+        }
+    }
 }
 
 const server = http.createServer(requestHandler)
